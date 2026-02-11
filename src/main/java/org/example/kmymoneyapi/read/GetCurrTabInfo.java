72127b2774cf7a8f59660ce506c17ee494d6ec48
James Rossini <james.rossini@posteo.de>
2026-02-11 10:28:57 +0100

    Changes after renaming of package.
    Cf. module "API (Core)", commit 5c7ce4f353904e81d8ebfea51ca18901667cfffa

--- src/main/java/org/example/kmymoneyapi/read/GetCurrTabInfo.java
+++ src/main/java/org/example/kmymoneyapi/read/GetCurrTabInfo.java
@@ -2,31 +2,31 @@ package org.example.kmymoneyapi.read;
 
 import java.io.File;
 
-import org.kmymoney.api.currency.ComplexPriceTable;
+import org.kmymoney.api.pricedb.ComplexPriceTable;
 import org.kmymoney.api.read.impl.KMyMoneyFileImpl;
 
 public class GetCurrTabInfo {
     // BEGIN Example data -- adapt to your needs
-    private static String gcshFileName = "example_in.xml";
+    private static String kmmFileName = "example_in.xml";
     // END Example data
 
     // -----------------------------------------------------------------
 
     public static void main(String[] args) {
-	try {
-	    GetCurrTabInfo tool = new GetCurrTabInfo();
-	    tool.kernel();
-	} catch (Exception exc) {
-	    System.err.println("Execution exception. Aborting.");
-	    exc.printStackTrace();
-	    System.exit(1);
-	}
+    	try {
+    		GetCurrTabInfo tool = new GetCurrTabInfo();
+    		tool.kernel();
+    	} catch (Exception exc) {
+    		System.err.println("Execution exception. Aborting.");
+    		exc.printStackTrace();
+    		System.exit(1);
+    	}
     }
 
     protected void kernel() throws Exception {
-	KMyMoneyFileImpl gcshFile = new KMyMoneyFileImpl(new File(gcshFileName));
+    	KMyMoneyFileImpl kmmFile = new KMyMoneyFileImpl(new File(kmmFileName));
 
-	ComplexPriceTable tab = gcshFile.getCurrencyTable();
-	System.out.println(tab.toString());
+    	ComplexPriceTable tab = kmmFile.getCurrencyTable();
+    	System.out.println(tab.toString());
     }
 }
